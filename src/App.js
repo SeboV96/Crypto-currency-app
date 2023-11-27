12f9bfdebab425ace6be858582a10f2ea03f19b6
SeboV96 <sebovilla96@gmail.com>
2023-11-27 13:02:19 -0300

    new components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import { Link, Route, Switch } from "react-router-dom"
-import {Layout, Typography, Space} from "antd"
-import { Navbar } from './components'
-import './App.css'
-
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Cryptocurrencies,
+  News,
+  Homepage,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
-      <div className='nabvar'>
-        <Navbar/>
+    <div className="app">
+      <div className="nabvar">
+        <Navbar />
       </div>
-      <div className='main'>
-
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
-      <div className='footer'>
-
-      </div>
+      <div className="footer"></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
